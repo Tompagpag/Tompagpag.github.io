e68ff03e7421ebc4f162969f68acf841b7838b36
Tom Paganel <paganel.tom@gmail.com>
2023-02-10 09:14:32 +0100

    code and design section product description, add input in footer and a little design

---
+++ src/quantity.js
@@ -0,0 +1,33 @@
+// Récupérer les éléments du DOM
+const input = document.getElementById("quantite");
+const minusSpan = input.previousElementSibling;
+const plusSpan = input.nextElementSibling;
+const initialPrice = parseInt(document.getElementById('price').innerText.match(/\d+/g), 10);
+
+// Callback pour augmenter le prix
+const addItemToTotalPrice = () => {
+  const itemPrice = document.getElementById('price');
+  let price = itemPrice.innerText.match(/\d+/g);
+  if (price = parseFloat(price[0]), 10) {
+    itemPrice.innerText = `$ ${price + initialPrice}`
+  }
+  let value = parseInt(input.value, 10);
+    value++;
+    input.value = value;
+}
+
+// Callback pour diminuer le prix
+const substractItemToTotalPrice = () => {
+  let value = parseInt(input.value, 10);
+  if(value > 1) {
+    value--;
+    input.value = value;
+    const itemPrice = document.getElementById('price');
+    let price = parseInt(itemPrice.innerText.match(/\d+/g), 10);
+    itemPrice.innerText = `$ ${price -  initialPrice}`
+  }
+}
+
+// Ecoute sur les clicks des spans + et -
+minusSpan.addEventListener('click', substractItemToTotalPrice);
+plusSpan.addEventListener('click', addItemToTotalPrice);
